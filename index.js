cdedd6f76660e3edddc364f82a448e1715784bdb
Aidana777 <aidananiazalieva27@gmail.com>
2022-01-13 12:57:09 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+/*const finger = prompt("Введите номер пальца: ");
+if(finger < 5){
+    alert ("Введённое число меньше пяти");
+}else{
+    alert("Введённое число больше пяти");
+}
+const cities1 = ["Bishkek", "London", "Tokyo", "Oslo", "Berlin"];
+const cities2 =cities1.push("Bishkek", "London", "Tokyo", "Oslo", "Berlin")
+console.log(cities2)*/ 
+
+const musics = [
+    "Do I wanna know",
+    "Baby I'm yours",
+    "Chandelier",
+    "Relax Take It Easy",
+    "Born To Die",
+    "Arcade"
+  ];
+  console.log(musics.includes("born"))
+  console.log(musics.includes("adb"))
+  
